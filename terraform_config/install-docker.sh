34af9147cc84c01d8ebe485585d529a7c46ace8e
root <root@localhost.localdomain>
2020-12-03 17:39:44 +0000

    3nd Commit. Modified bootstrap script to fit ansible annoying error.

--- terraform_config/install-docker.sh
+++ terraform_config/install-docker.sh
@@ -3,3 +3,8 @@ yum update
 yum install -y docker
 service docker start
 usermod -aG docker ec2-user
+yum install -y python37
+curl -O https://bootstrap.pypa.io/get-pip.py
+python3 get-pip.py --user
+export PATH=/root/.local/bin:$PATH
+pip install docker-compose
