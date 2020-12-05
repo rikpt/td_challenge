8ed9f6afc238f68e0409339434d230cb62d40a5b
root <root@localhost.localdomain>
2020-12-05 17:57:02 +0000

    5th commit. Change ReadMe and the ports on single container playbook

--- launch_td_challenge.sh
+++ launch_td_challenge.sh
@@ -5,7 +5,10 @@ terraform plan -var-file=main.auto.tfvars  -out=tfplan
 #terraform apply tfplan -auto-approve
 terraform apply tfplan
 
-#Ansible phase
-cd /home/talkdesk_challenge/ansible_config/
-ansible-playbook -i hosts run_mario.yaml -e 'ansible_python_interpreter=/usr/bin/python3'
+#Ansible phase - single container instance
+#cd /home/talkdesk_challenge/ansible_config/
+#ansible-playbook -i hosts run_mario.yaml -e 'ansible_python_interpreter=/usr/bin/python3'
 
+#Ansible phase - replica with docker service instance
+cd /home/talkdesk_challenge/ansible_config_replica/
+ansible-playbook -i hosts run_mario_replica.yaml -e 'ansible_python_interpreter=/usr/bin/python3'
